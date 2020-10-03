90b0a11550bb6a25e8268e01805f5213c3d033d4
Bibek Saini <itsbibeksaini@gmail.com>
2020-10-03 15:01:49 +0530

    Policy api :: Applying db migrations on server startup

--- PolicyApi/Program.cs
+++ PolicyApi/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PolicyApi.Services.SQLServer;
 
 namespace PolicyApi
 {
@@ -13,7 +16,17 @@ namespace PolicyApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetService<PolicyStore>();
+
+                context.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
