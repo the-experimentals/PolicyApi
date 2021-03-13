873ac81251ee27ac9ae78ae4e4804eabd011c05e
Bibek Saini <itsbibeksaini@gmail.com>
2021-03-13 20:35:04 +0530

    Adding gRPC services

---
+++ PolicyApi/Mappings/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using PolicyApi.ResponseModel;
+
+namespace PolicyApi.Mappings
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<TokenResponse, Protos.TokenResponse>()
+                .ForMember(dest => dest.ACCESS, opt => opt.MapFrom(src => src.ACCESS));                
+        }
+    }
+}
