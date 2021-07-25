b6995ab9ea806b4a1f25c1f2996088e8b409338e
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-25 13:43:50 +0530

    Adding policy controller

---
+++ PolicyApi/Controllers/PolicyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PolicyApi.Controllers
+{
+    [Route("policy")]
+    [ApiController]
+    public class PolicyController : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Testing policy api endpoint");
+        }
+    }
+}
