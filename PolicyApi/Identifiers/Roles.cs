f4585507c58a865a82396343cd1082ab7ac3dd11
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-25 13:33:54 +0530

    Deleting old project

--- PolicyApi/Identifiers/Roles.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace PolicyApi.Identifiers
-{
-    public class Roles
-    {
-        public const string ADMIN = "administrator";
-        public const string USER = "user";
-    }
-}
--- PolicyApi/ResponseModel/TokenResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace PolicyApi.ResponseModel
-{
-    public class TokenResponse
-    {
-        public string ACCESS { get; set; }
-    }
-}
