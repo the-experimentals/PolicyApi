a27add9e8961acdbaed16e8cd67a3be9751731ef
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-25 19:55:25 +0530

    Update

--- PolicyApi/Startup.cs
+++ PolicyApi/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using PolicyApi.Policy;
 using PolicyApi.Services.SQLServer;
 
@@ -27,9 +21,10 @@ namespace PolicyApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
 
+            services.AddMemoryCache();
+            services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
             services.AddDbContext<PolicyStore>(options => options.UseSqlServer(Configuration.GetConnectionString("PolicyStoreConnectionString")));
 
             services.AddSingleton<IPolicyManager, PolicyManager>();
