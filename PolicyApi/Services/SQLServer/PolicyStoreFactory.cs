5b08b2ec0b01d65bfb1f8774f7db8f3ee2db1e2e
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-25 19:35:05 +0530

    Adding policy store

---
+++ PolicyApi/Services/SQLServer/PolicyStoreFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace PolicyApi.Services.SQLServer
+{
+    public class PolicyStoreFactory : IDesignTimeDbContextFactory<PolicyStore>
+    {
+        public PolicyStore CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<PolicyStore>();
+
+            var connectionString = configuration.GetConnectionString("PolicyStoreConnectionString");
+
+            builder.UseSqlServer(connectionString);
+
+            return new PolicyStore(builder.Options);
+        }
+    }
+}
--- PolicyApi/Startup.cs
+++ PolicyApi/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PolicyApi.Services.SQLServer;
 
 namespace PolicyApi
 {
@@ -26,6 +28,8 @@ namespace PolicyApi
         {
 
             services.AddControllers();
+
+            services.AddDbContext<PolicyStore>(options => options.UseSqlServer(Configuration.GetConnectionString("PolicyStoreConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
