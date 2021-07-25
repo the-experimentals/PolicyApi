71b3c71edecd000b9e5e54dc902eb754c5519be2
Bibek Saini <itsbibeksaini@gmail.com>
2021-07-25 19:19:33 +0530

    Adding models

---
+++ PolicyApi/DataModels/Permissions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PolicyApi.DataModels
+{
+    public class Permissions
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required]
+        public string CODE { get; set; }
+        [Required]
+        public string NAME { get; set; }
+        [Required]
+        public string DISPLAY_NAME { get; set; }
+        public string DESCRIPTION { get; set; }
+
+        [Required]
+        public string PERMISSION_CATEDGORY_ID { get; set; }
+
+        [Required]
+        public int POSITION { get; set; }
+
+        [ForeignKey("PERMISSION_CATEDGORY_ID")]
+        public virtual PermissionCategories PERMISSION_CATEGORIES { get; set; }
+    }
+}
